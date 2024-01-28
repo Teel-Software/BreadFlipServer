2687e924761345a7b0f75f5e8300b777fd4053a2
Etien <stepan.filippov@yandex.ru>
2024-01-28 21:23:53 +0500

    different handlers for cors

--- internal/router/router.go
+++ internal/router/router.go
@@ -31,13 +31,13 @@ func NewRouter(host, port string) *Router {
 }
 
 func (ro *Router) StartRouter() {
-	ro.Router.HandleFunc("/hello", ro.helloHandler())
-	ro.Router.HandleFunc("/db", ro.dbHandler())
-	ro.Router.HandleFunc("/getrecords", ro.getRecordsHandler())
-	ro.Router.HandleFunc("/add", ro.addPlayerHandler())
-	ro.Router.HandleFunc("/change", ro.changePlayerHandler())
-	ro.Router.HandleFunc("/player/{id:[0-9]+}", ro.getPlayerHandler())
-	ro.Router.HandleFunc("/site", ro.siteHandler())
+	ro.Router.HandleFunc("/hello", ro.helloHandler()).Methods("GET", "OPTIONS", "POST", "PUT")
+	ro.Router.HandleFunc("/db", ro.dbHandler()).Methods("GET", "OPTIONS", "POST", "PUT")
+	ro.Router.HandleFunc("/getrecords", ro.getRecordsHandler()).Methods("GET", "OPTIONS", "POST", "PUT")
+	ro.Router.HandleFunc("/add", ro.addPlayerHandler()).Methods("GET", "OPTIONS", "POST", "PUT")
+	ro.Router.HandleFunc("/change", ro.changePlayerHandler()).Methods("GET", "OPTIONS", "POST", "PUT")
+	ro.Router.HandleFunc("/player/{id:[0-9]+}", ro.getPlayerHandler()).Methods("GET", "OPTIONS", "POST", "PUT")
+	ro.Router.HandleFunc("/site", ro.siteHandler()).Methods("GET", "OPTIONS", "POST", "PUT")
 	http.ListenAndServe(fmt.Sprintf("%s:%s", ro.Host, ro.Port), ro.Router)
 	log.Default().Println("Server started")
 }
@@ -45,7 +45,8 @@ func (ro *Router) StartRouter() {
 func (ro *Router) helloHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Add("Access-Control-Allow-Headers", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "*")
 		log.Default().Println("handling hello")
 		io.WriteString(w, "hello")
 	}
@@ -53,91 +54,121 @@ func (ro *Router) helloHandler() http.HandlerFunc {
 
 func (ro *Router) dbHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		log.Default().Println("handling top ten")
-		topRecords := ro.Database.GetTopTenRecords()
-		io.WriteString(w, topRecords)
+		switch r.Method {
+		case "OPTIONS":
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			w.Header().Add("Access-Control-Allow-Headers", "*")
+			w.Header().Add("Access-Control-Allow-Methods", "*")
+		default:
+			log.Default().Println("handling top ten")
+			topRecords := ro.Database.GetTopTenRecords()
+			io.WriteString(w, topRecords)
+		}
 	}
 }
 
 func (ro *Router) getRecordsHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		log.Default().Println("handling get page")
-		offset, err := strconv.Atoi(r.Header.Get("offset"))
-		if err != nil {
-			log.Default().Printf("cant parse offset %+v\n", err)
-		}
-		count, err := strconv.Atoi(r.Header.Get("count"))
-		if err != nil {
-			log.Default().Printf("cant parse count %+v\n", err)
-		}
+		switch r.Method {
+		case "OPTIONS":
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			w.Header().Add("Access-Control-Allow-Headers", "*")
+			w.Header().Add("Access-Control-Allow-Methods", "*")
+		default:
+			log.Default().Println("handling get page")
+			offset, err := strconv.Atoi(r.Header.Get("offset"))
+			if err != nil {
+				log.Default().Printf("cant parse offset %+v\n", err)
+			}
+			count, err := strconv.Atoi(r.Header.Get("count"))
+			if err != nil {
+				log.Default().Printf("cant parse count %+v\n", err)
+			}
 
-		recordList := ro.Database.GetRecordsWithPaging(offset, count)
+			recordList := ro.Database.GetRecordsWithPaging(offset, count)
 
-		marshalledRecords, err := json.Marshal(recordList)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "json failed: %v\n", err)
+			marshalledRecords, err := json.Marshal(recordList)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "json failed: %v\n", err)
+			}
+			io.WriteString(w, string(marshalledRecords))
 		}
-		io.WriteString(w, string(marshalledRecords))
 	}
 }
 
 func (ro *Router) addPlayerHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		log.Default().Println("handling add player")
-		newPlayerBytes := make([]byte, r.ContentLength)
-		_, err := r.Body.Read(newPlayerBytes)
-		if err != nil && err != io.EOF {
-			log.Default().Printf("f put request %+v\n", err)
+		switch r.Method {
+		case "OPTIONS":
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			w.Header().Add("Access-Control-Allow-Headers", "*")
+			w.Header().Add("Access-Control-Allow-Methods", "*")
+		default:
+			log.Default().Println("handling add player")
+			newPlayerBytes := make([]byte, r.ContentLength)
+			_, err := r.Body.Read(newPlayerBytes)
+			if err != nil && err != io.EOF {
+				log.Default().Printf("f put request %+v\n", err)
+			}
+
+			id := ro.Database.AddPlayer(newPlayerBytes)
+			log.Default().Printf("ok put request %s\n", string(newPlayerBytes))
+
+			io.WriteString(w, strconv.Itoa(id))
 		}
-
-		id := ro.Database.AddPlayer(newPlayerBytes)
-		log.Default().Printf("ok put request %s\n", string(newPlayerBytes))
-
-		io.WriteString(w, strconv.Itoa(id))
 	}
 }
 
 func (ro *Router) changePlayerHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		log.Default().Println("handling change player")
-		reqBody := make([]byte, r.ContentLength)
-		_, err := r.Body.Read(reqBody)
-		if err != nil && err != io.EOF {
-			log.Default().Printf("failed change player request %+v\n", err)
-			return
-		}
+		switch r.Method {
+		case "OPTIONS":
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			w.Header().Add("Access-Control-Allow-Headers", "*")
+			w.Header().Add("Access-Control-Allow-Methods", "*")
+		default:
+			log.Default().Println("handling change player")
+			reqBody := make([]byte, r.ContentLength)
+			_, err := r.Body.Read(reqBody)
+			if err != nil && err != io.EOF {
+				log.Default().Printf("failed change player request %+v\n", err)
+				return
+			}
 
-		ro.Database.ChangeRecordForPlayer(reqBody)
-		log.Default().Printf("ok change player request %s\n", string(reqBody))
+			ro.Database.ChangeRecordForPlayer(reqBody)
+			log.Default().Printf("ok change player request %s\n", string(reqBody))
+		}
 	}
 }
 
 func (ro *Router) getPlayerHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		log.Default().Println("handling get player")
-		id, _ := strconv.Atoi(mux.Vars(r)["id"])
-		log.Default().Printf("id requested %d\n", id)
-		ans := ro.Database.GetPlayerRecord(id)
-		io.WriteString(w, ans)
+		switch r.Method {
+		case "OPTIONS":
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			w.Header().Add("Access-Control-Allow-Headers", "*")
+			w.Header().Add("Access-Control-Allow-Methods", "*")
+		default:
+			log.Default().Println("handling get player")
+			id, _ := strconv.Atoi(mux.Vars(r)["id"])
+			log.Default().Printf("id requested %d\n", id)
+			ans := ro.Database.GetPlayerRecord(id)
+			io.WriteString(w, ans)
+		}
 	}
 }
 
 func (ro *Router) siteHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		tpl := template.Must(template.ParseFiles("index.html"))
-		log.Default().Println("handling site")
-		tpl.Execute(w, ro.Database.GetRecordsWithPaging(0, 100).List)
+		switch r.Method {
+		case "OPTIONS":
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			w.Header().Add("Access-Control-Allow-Headers", "*")
+			w.Header().Add("Access-Control-Allow-Methods", "*")
+		default:
+			tpl := template.Must(template.ParseFiles("index.html"))
+			log.Default().Println("handling site")
+			tpl.Execute(w, ro.Database.GetRecordsWithPaging(0, 100).List)
+		}
 	}
 }
